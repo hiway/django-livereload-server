8b2ea894e3cb6c2e49a5189c4f1cdc6165bb9663
Harshad Sharma <harshad@sharma.io>
2017-01-22 23:22:06 +0530

    Add LIVERELOAD_PROXY_HOST, LIVERELOAD_PROXY_PORT, LIVERELOAD_PROXY_SECURE settings.

--- livereload/__init__.py
+++ livereload/__init__.py
@@ -16,3 +16,18 @@ def livereload_port():
 def livereload_host():
     from django.conf import settings
     return getattr(settings, 'LIVERELOAD_HOST', '127.0.0.1')
+
+
+def livereload_proxy_port():
+    from django.conf import settings
+    return int(getattr(settings, 'LIVERELOAD_PROXY_PORT', None))
+
+
+def livereload_proxy_host():
+    from django.conf import settings
+    return getattr(settings, 'LIVERELOAD_PROXY_HOST', None)
+
+
+def livereload_proxy_secure():
+    from django.conf import settings
+    return getattr(settings, 'LIVERELOAD_PROXY_SECURE', False) is True
